a75f7e83518b492d34f2bd39f0aa32e8a282a78e
Jamail <j.lavan@hotmail.com>
2020-03-12 15:00:26 -0700

    StockAPI completed with hard coded stock symbol, will change

--- businessdashboard/src/Containers/StockApi/StockApi.js
+++ businessdashboard/src/Containers/StockApi/StockApi.js
@@ -1,23 +1,75 @@
-const stockapi = () => {
+import  React, { Component } from 'react';
 
-var googleFinance = require('google-finance');
+import Plot from 'react-plotly.js';
 
-googleFinance.companyNews({
-    symbol: 'NASDAQ:AAPL'
-}, function (err, news) {
-    console.log(err)
-    console.log(news)
-});
+class Stockapi extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stockChartXValues: [],
+            stockChartYValues: []
+        }
+    }
 
-googleFinance.historical({
-    symbol: 'NASDAQ:AAPL',
-    from: '2014-01-01',
-    to: '2014-12-31'
-}, function (err, quotes) {
-    console.log(err)
-    console.log(quotes)
-});
+    componentDidMount() {
+        this.fetchStock();
+    }
 
+    fetchStock() {
+        const pointerToThis = this;
+        const API_KEY = 'Place API_Key here';
+        let StockSymbol = 'AMZN'
+        let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+        let stockChartXValuesFunction = [];
+        let stockChartYValuesFunction = [];
+
+        fetch(API_Call)
+            .then(
+                function (response) {
+                    return response.json();
+                }
+            )
+            .then(
+                function (data) {
+                    console.log(data)
+                    for (var key in data['Time Series (Daily)']) {
+                        stockChartXValuesFunction.push(key);
+                        stockChartYValuesFunction.push(data['Time Series (Daily)']
+                        [key]['1. open']);
+                    }
+
+                    console.log(stockChartXValuesFunction)
+                    pointerToThis.setState({
+                        stockChartXValues: stockChartXValuesFunction,
+                        stockChartYValues: stockChartYValuesFunction
+                    });
+                }
+            )
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Stock Market </h1>
+
+                <Plot
+                    data={[
+                        {
+                            x: this.state.stockChartXValues,
+                            y: this.state.stockChartYValues,
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: { color: 'red' },
+                        }
+                    ]}
+                    layout={{ width: 720, height: 440, title: "Monthly Analysis" }}
+                />
+
+
+            </div>
+        )
+    }
 }
 
-export default stockapi;
+export default Stockapi;
